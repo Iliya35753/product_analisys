0015c91f01eaa2d2912f0d8a11aff20cbb05c371
PelihovIliya <146917520+Iliya35753@users.noreply.github.com>
2025-04-18 17:42:16 +0300

    Update retention.sql

--- SQL/retention/retention.sql
+++ SQL/retention/retention.sql
@@ -1,7 +1,6 @@
 with t2 as (SELECT start_date,
                    date,
-                   round(count(distinct user_id)::decimal/max(count(distinct user_id)) OVER(PARTITION BY start_date),
-                         2) as retention
+                   round(count(distinct user_id)::decimal/max(count(distinct user_id)) OVER(PARTITION BY start_date), 2) as retention
             FROM   (SELECT user_id,
                            min(time::date) OVER (PARTITION BY user_id) as start_date,
                            time::date as date
@@ -10,6 +9,5 @@ with t2 as (SELECT start_date,
             ORDER BY start_date, date)
 SELECT date_trunc('month', start_date)::date as start_month,
        start_date,
-       extract('day'
-FROM   age(date, start_date))::int as day_number, retention
+       extract('day' FROM   age(date, start_date))::int as day_number, retention
 FROM   t2
